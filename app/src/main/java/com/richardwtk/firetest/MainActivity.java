18cdad5c7660aea1bff96fa63efb4686ba89e9fe
Richard <ricwtk@gmail.com>
2019-06-04 23:20:41 +0800

    add email/password authentication

--- app/src/main/java/com/richardwtk/firetest/MainActivity.java
+++ app/src/main/java/com/richardwtk/firetest/MainActivity.java
@@ -1,15 +1,27 @@
 package com.richardwtk.firetest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +30,62 @@ public class MainActivity extends AppCompatActivity {
 
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mAuth = FirebaseAuth.getInstance();
 
-        final Button button = findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button log_in_button = findViewById(R.id.loginbtn);
+        log_in_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Bundle bundle = new Bundle();
                 bundle.putString(FirebaseAnalytics.Param.ITEM_ID, getResources().getResourceEntryName(v.getId()));
                 mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.ADD_TO_WISHLIST, bundle);
             }
         });
+
+        final Button sign_up_button = findViewById(R.id.signupbtn);
+        sign_up_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        final TextView displaytext = findViewById(R.id.textdisplay);
+        if (user != null) displaytext.setText(user.getEmail());
+    }
+
+    private void signUp() {
+        final EditText emailtext = findViewById(R.id.emailtext);
+        final EditText passwordtext = findViewById(R.id.passwdtext);
+        final TextView displaytext = findViewById(R.id.textdisplay);
+
+        mAuth.createUserWithEmailAndPassword(emailtext.getText().toString(), passwordtext.getText().toString())
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("firetest", "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("firetest", "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                    }
+
+                    // ...
+                }
+            });
     }
 
 }
